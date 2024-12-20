a6d906d405e3d2d2c8a550f2f0dc7aa3b0547aaf
shoham1246 <shoham.marom@campus.technion.ac.il>
2024-12-20 12:42:18 +0200

    Update median.c 20.12
    Update median.c 20.12
    1. removed string.h
    2. removed print "median number is"
    3. correctrd no. line of error

--- median.c
+++ median.c
@@ -42,7 +42,7 @@ int main(int argc, char **argv) {
 		if(scanf_return == EOF){
 			break; //end of file
 		} else if (scanf_return !=1 || grade <0 || grade > 100){
-			fprintf(stderr, "Error at line %d: invalid input %d", num_of_grades, grade-1 ); //write input!!!!
+			fprintf(stderr, "Error at line %d: invalid input %d", num_of_grades+1, grade ); //write input!!!!
 			exit(1);
 		}
 		num_of_grades++;
